486c5d3e692822c1943cc0f63298c294d5686549
Rogerio Motitsuki <rogerio.motitsuki@gmail.com>
2016-06-22 20:34:27 -0300

    KVIQ: translation

--- evaluation/forms/kviq_form.py
+++ evaluation/forms/kviq_form.py
@@ -2,7 +2,7 @@
 from django import forms
 from ..widgets import ButtonRadioSelect
 from ..mixin import ControlNTFormMixin
-from ..models.kviq import CHOICES_VISUAL_IMAGES, CHOICES_CINE_IMAGES, CHOICES_DOMINANT_LIMB
+from ..models.kviq import KVIQ, CHOICES_VISUAL_IMAGES, CHOICES_CINE_IMAGES, CHOICES_DOMINANT_LIMB
 
 TEXT_VISUAL_IMAGES = u"""
 <ol class="ol-container">
@@ -17,79 +17,104 @@ TEXT_VISUAL_IMAGES = u"""
 class KVIQForm(ControlNTFormMixin, forms.ModelForm):
     imrea_nt = {'fields': ('neck_visual_images', 'neck_cine_images'),}
 
-    dominant_limb = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                           choices=CHOICES_DOMINANT_LIMB)
+    def __init__(self, *args, **kwargs):
+        super(KVIQForm, self).__init__(*args, **kwargs)
 
-    neck_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                           choices=CHOICES_VISUAL_IMAGES)
-    neck_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                         choices=CHOICES_CINE_IMAGES)
-    shoulder_elevation_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                         choices=CHOICES_VISUAL_IMAGES)
-    shoulder_elevation_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                       choices=CHOICES_CINE_IMAGES)
-    shoulder_flexion_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                           choices=CHOICES_VISUAL_IMAGES)
-    shoulder_flexion_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                         choices=CHOICES_CINE_IMAGES)
-    shoulder_flexion_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                               choices=CHOICES_VISUAL_IMAGES)
-    shoulder_flexion_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                             choices=CHOICES_CINE_IMAGES)
-    elbow_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                choices=CHOICES_VISUAL_IMAGES)
-    elbow_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                              choices=CHOICES_CINE_IMAGES)
-    elbow_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                    choices=CHOICES_VISUAL_IMAGES)
-    elbow_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                  choices=CHOICES_CINE_IMAGES)
-    thumb_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                choices=CHOICES_VISUAL_IMAGES)
-    thumb_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                              choices=CHOICES_CINE_IMAGES)
-    thumb_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                    choices=CHOICES_VISUAL_IMAGES)
-    thumb_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                  choices=CHOICES_CINE_IMAGES)
-    trunk_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                            choices=CHOICES_VISUAL_IMAGES)
-    trunk_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                          choices=CHOICES_CINE_IMAGES)
-    knee_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                               choices=CHOICES_VISUAL_IMAGES)
-    knee_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                             choices=CHOICES_CINE_IMAGES)
-    knee_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                   choices=CHOICES_VISUAL_IMAGES)
-    knee_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                 choices=CHOICES_CINE_IMAGES)
-    hip_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                              choices=CHOICES_VISUAL_IMAGES)
-    hip_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                            choices=CHOICES_CINE_IMAGES)
-    hip_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                  choices=CHOICES_VISUAL_IMAGES)
-    hip_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                choices=CHOICES_CINE_IMAGES)
-    foot_tapping_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                       choices=CHOICES_VISUAL_IMAGES)
-    foot_tapping_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                     choices=CHOICES_CINE_IMAGES)
-    foot_tapping_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                           choices=CHOICES_VISUAL_IMAGES)
-    foot_tapping_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                         choices=CHOICES_CINE_IMAGES)
-    foot_rotation_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                        choices=CHOICES_VISUAL_IMAGES)
-    foot_rotation_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                      choices=CHOICES_CINE_IMAGES)
-    foot_rotation_not_dom_visual_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                            choices=CHOICES_VISUAL_IMAGES)
-    foot_rotation_not_dom_cine_images = forms.ChoiceField(widget=ButtonRadioSelect(),
-                                                          choices=CHOICES_CINE_IMAGES)
+        self.fields['dominant_limb'].choices = CHOICES_DOMINANT_LIMB
+
+        self.fields['neck_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['neck_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['shoulder_elevation_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['shoulder_elevation_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['shoulder_flexion_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['shoulder_flexion_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['shoulder_flexion_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['shoulder_flexion_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['elbow_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['elbow_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['elbow_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['elbow_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['thumb_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['thumb_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['thumb_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['thumb_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['trunk_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['trunk_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['knee_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['knee_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['knee_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['knee_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['hip_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['hip_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['hip_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['hip_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['foot_tapping_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['foot_tapping_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['foot_tapping_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['foot_tapping_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['foot_rotation_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['foot_rotation_dom_cine_images'].choices = CHOICES_CINE_IMAGES
+
+        self.fields['foot_rotation_not_dom_visual_images'].choices = CHOICES_VISUAL_IMAGES
+        self.fields['foot_rotation_not_dom_cine_images'].choices = CHOICES_CINE_IMAGES
 
     class Media:
         css = {
             'all': ('css/evaluation_forms.css',)
+        }
+
+    class Meta:
+        model = KVIQ
+        fields = ['patient', 'period', ]
+        widgets = {
+            'dominant_limb': ButtonRadioSelect(),
+            'neck_visual_images': ButtonRadioSelect(),
+            'neck_cine_images': ButtonRadioSelect(),
+            'shoulder_elevation_visual_images': ButtonRadioSelect(),
+            'shoulder_elevation_cine_images': ButtonRadioSelect(),
+            'shoulder_flexion_dom_visual_images': ButtonRadioSelect(),
+            'shoulder_flexion_dom_cine_images': ButtonRadioSelect(),
+            'shoulder_flexion_not_dom_visual_images': ButtonRadioSelect(),
+            'shoulder_flexion_not_dom_cine_images': ButtonRadioSelect(),
+            'elbow_dom_visual_images': ButtonRadioSelect(),
+            'elbow_dom_cine_images': ButtonRadioSelect(),
+            'elbow_not_dom_visual_images': ButtonRadioSelect(),
+            'elbow_not_dom_cine_images': ButtonRadioSelect(),
+            'thumb_dom_visual_images': ButtonRadioSelect(),
+            'thumb_dom_cine_images': ButtonRadioSelect(),
+            'thumb_not_dom_visual_images': ButtonRadioSelect(),
+            'thumb_not_dom_cine_images': ButtonRadioSelect(),
+            'trunk_visual_images': ButtonRadioSelect(),
+            'trunk_cine_images': ButtonRadioSelect(),
+            'knee_dom_visual_images': ButtonRadioSelect(),
+            'knee_dom_cine_images': ButtonRadioSelect(),
+            'knee_not_dom_visual_images': ButtonRadioSelect(),
+            'knee_not_dom_cine_images': ButtonRadioSelect(),
+            'hip_dom_visual_images': ButtonRadioSelect(),
+            'hip_dom_cine_images': ButtonRadioSelect(),
+            'hip_not_dom_visual_images': ButtonRadioSelect(),
+            'hip_not_dom_cine_images': ButtonRadioSelect(),
+            'foot_tapping_dom_visual_images': ButtonRadioSelect(),
+            'foot_tapping_dom_cine_images': ButtonRadioSelect(),
+            'foot_tapping_not_dom_visual_images': ButtonRadioSelect(),
+            'foot_tapping_not_dom_cine_images': ButtonRadioSelect(),
+            'foot_rotation_dom_visual_images': ButtonRadioSelect(),
+            'foot_rotation_dom_cine_images': ButtonRadioSelect(),
+            'foot_rotation_not_dom_visual_images': ButtonRadioSelect(),
+            'foot_rotation_not_dom_cine_images': ButtonRadioSelect()
         }
