8abe4920da9997712b80699a40a9f7aa9e44d3a7
Rogerio Motitsuki <rogerio.motitsuki@gmail.com>
2016-07-12 19:42:02 -0300

    Patient: new demografic datas Patient: i18n Epworth: i18n

--- evaluation/admin/patient_admin.py
+++ evaluation/admin/patient_admin.py
@@ -5,6 +5,41 @@ from evaluation.models.patient import Patient
 
 
 class PatientAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_(u'Patient'), {
+            'fields': ('name', 'birthdate'),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+        (_(u'Social characteristics'), {
+            'fields': ('age', 'gender', 'marital_status', 'profession', 'occupation',
+                       'schooling_level', 'social_insurance_status', 'transportation_status'),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+        (_(u'Family support'), {
+            'fields': ('fammily_ref_therapy', 'fammily_ref_home', 'fammily_composition',
+                       'quantity_ppl_home'),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+        (_(u'Family support'), {
+            'fields': ('fammily_ref_therapy', 'fammily_ref_home', 'fammily_composition',
+                       'quantity_ppl_home'),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+        (_(u'Economic aspects'), {
+            'fields': ('family_income', 'family_income_average',),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+        (_(u'Demographic data'), {
+            'fields': ('origin_city', 'conducting_source',),
+            'description': {'fieldset': '_1col_panel'}
+        }),
+    )
+
+
+
+
+
+
     # for removing add another option
     def has_add_permission(self, request):
         return True
--- evaluation/models/patient.py
+++ evaluation/models/patient.py
@@ -2,19 +2,42 @@
 from django.db import models
 from django.forms import extras
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import pgettext
 from .period import Period
 
 
 class Patient(models.Model):
 
-    name = models.CharField(max_length=50)
-    birthdate = models.DateField(blank=True, null=True)
+    name = models.CharField(_('Name'), max_length=50)
+    birthdate = models.DateField(_('Date of birth'), blank=True, null=True)
+    # Caracterização social do paciente:
+    age = models.IntegerField(pgettext('Patient', u'Age'), blank=True, null=True)
+    gender = models.CharField(pgettext('Patient', u'Gender'), max_length=100, blank=True, null=True)
+    marital_status = models.CharField(pgettext('Patient', u'Marital status'), max_length=100, blank=True, null=True)
+    profession = models.CharField(pgettext('Patient', u'Profession'), max_length=100, blank=True, null=True)
+    occupation = models.CharField(pgettext('Patient', u'Occupation/work'), max_length=100, blank=True, null=True)
+    schooling_level = models.CharField(pgettext('Patient', u'Schooling level'), max_length=100, blank=True, null=True)
+    social_insurance_status = models.CharField(pgettext('Patient', u'Social insurance status'), max_length=100, blank=True, null=True)
+    transportation_status = models.CharField(pgettext('Patient', u'Social insurance status'), max_length=100, blank=True, null=True)
+    # Apoio familiar:
+    fammily_ref_therapy = models.CharField(pgettext('Patient', u'Family member reference for therapy frequency'), max_length=100, blank=True, null=True)
+    fammily_ref_home = models.CharField(pgettext('Patient', u'Family member reference at home'), max_length=100, blank=True, null=True)
+    fammily_composition = models.CharField(pgettext('Patient', u'Family composition'), max_length=255, blank=True, null=True)
+    quantity_ppl_home = models.IntegerField(pgettext('Patient', u'Number of people at home'), blank=True, null=True)
+    # Aspectos econômicos:
+    family_income = models.IntegerField(pgettext('Patient', u'Family income'), blank=True, null=True)
+    family_income_average = models.IntegerField(pgettext('Patient', u'Average family income'), blank=True, null=True)
+    # Dados demográficos:
+    origin_city = models.CharField(pgettext('Patient', u'City of origin'), max_length=100, blank=True, null=True)
+    conducting_source = models.CharField(pgettext('Patient', u'Conducting source'), max_length=100, blank=True, null=True)
 
     def __unicode__(self):
         return self.name
     
     class Meta:
         ordering = ['name']
+        verbose_name = _('Patient')
+        verbose_name_plural = _('Patients')
 
     def find_evaluated_period(self):
         periods = Period._find_evaluated_period(self)
