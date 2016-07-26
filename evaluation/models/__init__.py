b917e42c532330fdf610b440817398b42468a978
Rogerio Motitsuki <rogerio.motitsuki@gmail.com>
2016-07-26 19:35:37 -0300

    Merge branch 'master' of https://github.com/ansp-2015/imrea

--- evaluation/models/__init__.py
+++ evaluation/models/__init__.py
@@ -1,22 +1,11 @@
 # -*- coding: utf-8 -*-
 from .base_evaluation import BaseEvaluation
 from .patient import Patient
-from .kviq import KVIQ
-from .sis import SIS
-from .bostonaphasia import BostonAphasia
-from .eeg import Eeg, EegFile
-from .fim import FIM
-from .clin import Clin
-from .had import HAD
-from .hamilton import Hamilton
-from .fuglmeyer import FuglMeyer
-from .mmse import MMSE
-from .epworth import Epworth
-
 from .period import Period
 
-
-__all__ = ['BaseEvaluation', 'Patient', 'KVIQ', 'SIS', 'FIM', 'Eeg', 'EegFile', 'BostonAphasia', 'Clin', 'Period', 'HAD',
-           'Hamilton', 'FuglMeyer', 'MMSE']
-
+__all__ = ['BaseEvaluation', 'Patient', 'Period']
+for c in BaseEvaluation.__subclasses__():
+    cname = c.__name__
+    __all__.append(cname)
+    exec('from .%s import %s' % (cname.lower(), cname))
 
